96761e337efd799dcaa38c1b924167f2bdc10096
Maxthod <maxthod@pm.me>
2021-11-21 23:17:15 -0500

    Continue...

--- main.go
+++ main.go
@@ -2,60 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/duvalhub/cloudconfigclient"
-	"github.com/mitchellh/mapstructure"
 )
 
-type Change struct {
-	key   string
-	value string
-}
-
-type File struct {
-	name    string
-	changes []Change
-}
-
-type App struct {
-	hello  string
-	bybye  string
-	mapped string
-}
-
-func mapFromConfig(config cloudconfigclient.Source) File {
-	// j :=config.PropertySources[0]
-	// y, err := yaml.JSONToYAML(j)
-	// if err != nil {
-	// fmt.Printf("err: %v\n", err)
-	// return File{}
-	// }
-	// fmt.Println(string(y))
-	// return File{}
-	// var files []File = []File
-	app := App{}
-	mapstructure.Decode(config, &app)
-
-	fmt.Printf("%v\n", app)
-
-	// aasd := config.Get("files", "notfound")
-	// fmt.Printf("\n\n%v\n", aasd)
-	// for _, file := range aasd {
-	// 	fmt.Printf("key=%s, value=%s\n", file.key, file.value)
-	// }
-
-	return File{}
-}
-
 func main() {
-	config, err := ReadFromEnv().Load()
+	configsEnv := cloudconfigclient.ConfigsEnv{}
+	config, err := configsEnv.ReadFromEnv().Load()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("%+v\n", config)
-	port := config.Get("server.port", "").(float64)
-	fmt.Println(port)
-	Work(config)
-	// mapFromConfig(config)
+	filesToChange, err := filesToChange(config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, file := range filesToChange {
+		file.Apply()
+	}
+
+	log.Print("Done. Files processed.")
 }
--- write_test.go
+++ write_test.go
@@ -1,21 +1,56 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
 	"testing"
-	"text/template"
 )
 
-type Inventory struct {
-	Material string
-	Count    uint
-}
-
 func TestWrite(t *testing.T) {
+	// Arrange
+	tmpFile, err := ioutil.TempFile("", "test-write")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name())
+	source, err := os.Open("testdata/write_input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer source.Close()
 
-	sweaters := Inventory{"wool", 17}
-	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
-	if err != nil { panic(err) }
-	err = tmpl.Execute(os.Stdout, sweaters)
-	if err != nil { panic(err) }
+	nBytes, err := io.Copy(tmpFile, source)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Copied '%d' bytes", nBytes)
+
+	fileToChange := FileToChange{tmpFile.Name(), map[string]Replacement{
+		"url": {
+			"CONFIG_URL",
+			"http://myurl.com",
+		},
+	}}
+
+	// Act
+	fileToChange.Apply()
+
+	// Assert
+	fmt.Println(fileToChange)
+	expected, err := os.ReadFile("testdata/write_expected")
+	if err != nil {
+		log.Fatal(err)
+	}
+	output, err := os.ReadFile(tmpFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !bytes.Equal(expected, output) {
+		t.Errorf("fileToChange.Apply() = \n%v\n Want \n%v\n", string(output), string(expected))
+	}
 }
